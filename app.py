147e5ae3a45c92d044b34578918011396003e65b
Ahad <ahadnaseer47@gmail.com>
2025-08-04 04:58:58 +0500

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Add favicon route to prevent 404 errors
+@app.route('/favicon.ico')
+def favicon():
+    return '', 204
+
+if __name__ == '__main__':
+    app.run(debug=True)
